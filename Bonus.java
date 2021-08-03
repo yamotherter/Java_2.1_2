2349d3c4a41bd589d8f1d61daf3f6b6350b00f56
Daria Peydus <dasha_vishnya@protonmail.com>
2021-08-03 15:19:46 +0300

    Bonus

---
+++ Bonus.java
@@ -0,0 +1,19 @@
+public class Bonus {
+    public static void main(String[] args) {
+
+        // Задаем исходный баланс телефонного счета
+        // и сумму пополнения
+        float balance = 546F;
+        float income = 1600.56F;
+
+        // Бонусы зачисляются при пополнении свыше 100 рублей
+        int bonus = (income > 1000) ? ((int)income / 100) : 0;
+
+        // Итоговый баланс после пополнения
+        balance += income + bonus;
+
+        // Вывод итогового баланса и сумму бонуса
+        System.out.println("Balance: " + balance);
+        System.out.print("Bonus: " + bonus);
+    }
+}
